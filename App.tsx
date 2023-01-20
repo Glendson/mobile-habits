77d752bac6a5cfeebb785dff127917da6ba517d2
Glendson <gztomazetto@gmail.com>
2023-01-20 22:48:34 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+
+  const [fontsLoded] = useFonts({
+    Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold
+  })
+
+  if (!fontsLoded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
