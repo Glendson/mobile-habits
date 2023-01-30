6d2e5b3b514e496a19914d2fb833265eef58895e
Glendson <gztomazetto@gmail.com>
2023-01-30 18:36:52 +0000

    Screens Updated

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Dimensions } from "react-native";
+import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
 
 const week_days = 7
 const screen_horizontal_padding = (32 * 2) / 5
@@ -6,12 +6,15 @@ const screen_horizontal_padding = (32 * 2) / 5
 export const DAY_MARGIN_BETWEEN = 8
 export const DAY_SIZE = (Dimensions.get("screen").width / week_days) - (screen_horizontal_padding + 5)
 
-export function HabitDay() {
+interface Props extends TouchableOpacityProps {}
+
+export function HabitDay({...rest}: Props) {
     return (
         <TouchableOpacity
             className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
             style={{ width: DAY_SIZE, height: DAY_SIZE }}
             activeOpacity={0.7}
+            { ...rest }
         />
     )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,9 @@ const minimunSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
+
+    const { navigate } = useNavigation()
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -24,7 +28,7 @@ export function Home() {
             <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 100 }}>
                 <View className="flex-row flex-wrap">
                     {datesFromYearStart.map(date => (
-                        <HabitDay key={date.toISOString()} />
+                        <HabitDay key={date.toISOString()} onPress={() => navigate("habit", {date: date.toISOString()})} />
                     ))}
 
                     {
