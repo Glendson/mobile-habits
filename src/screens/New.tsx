de616449d289c766afb32fe49d660510594e47bc
Glendson <gztomazetto@gmail.com>
2023-02-04 17:52:37 +0000

    ...

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -24,7 +24,7 @@ export function New() {
     async function handleCreateNewHabit(){
         try{
             if(!title.trim() || weekDays.length === 0) {
-                Alert.alert("Novo habito", "Informe o nome do habito e escolha a periodicidade.")
+                return Alert.alert("Novo habito", "Informe o nome do habito e escolha a periodicidade.")
             }
             
             await api.post("/habits", { title, weekDays })
